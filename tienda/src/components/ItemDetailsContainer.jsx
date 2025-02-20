2da5b765bc3e65553cb327a2a075410bee3285d4
sandor <sandormann1@hotmail.com>
2025-02-20 08:48:19 -0600

    Entrega 2 4v

--- tienda/src/components/ItemDetailsContainer.jsx
+++ tienda/src/components/ItemDetailsContainer.jsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router'
+
 export default function ItemDetailsContainer(){
+    const [ detail,setDetail ] = useState();
+    const { id } = useParams();
+
+    useEffect(()=>{
+        fetch(`https://fakestoreapi.com/products/${id}`)
+        .then( res => res.json())
+        .then( res => setDetail(res))
+        .then(console.log)
+    },[id])
+
     return(
-        <div>ItemDetailsContainer</div>
+        <>
+            <div>Producto</div>
+            <h2>{detail?.title}</h2>
+            <img src={detail?.image}/>
+            <p>{detail?.description}</p>
+            <p>$ {detail?.price}</p>
+        </>
     )
 }
