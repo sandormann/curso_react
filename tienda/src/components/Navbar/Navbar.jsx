4a36f07ae1637f0dd06c5c22f9a5dcab4170afb6
sandor <sandormann1@hotmail.com>
2025-02-21 20:01:32 -0600

    Entrega 2 6v

--- tienda/src/components/Navbar/Navbar.jsx
+++ tienda/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import { NavLink, Link } from 'react-router'
 import styles from './Navbar.module.css'
+import CartWidget from '../CartWidget/CartWidget'
+import Logo from '../Logo/Logo'
 export default function Navbar(){
     return(
         <nav className={styles.nav }>
+            <Logo /> 
             <NavLink to="/categories/electronics" className={styles.link}>Electronics</NavLink>
             <NavLink to="/categories/jewelery" className={styles.link}>Jewelery</NavLink>
             <NavLink to="/categories/men's clothing" className={styles.link}>Men's clothing</NavLink>
             <NavLink to="/categories/women's clothing" className={styles.link}>Women's clothing</NavLink>
+            <CartWidget />
         </nav>
     )
 }
